8e104bffea8e644a902f4d442072b8d1c389fd57
putratk <putratk1@gmail.com>
2016-10-16 13:52:38 +0700

    edit text

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl3027/applaundry/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl3027/applaundry/MainActivity.java
@@ -2,12 +2,73 @@ package id.sch.smktelkom_mlg.tugas01.xiirpl3027.applaundry;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etNama, etAlamat, etNomor;
+    Button btOk;
+    TextView tvHasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        etAlamat = (EditText) findViewById(R.id.editTextAlamat);
+        etNomor = (EditText) findViewById(R.id.editTextNomor);
+        btOk = (Button) findViewById(R.id.buttonOk);
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+
+        findViewById(R.id.buttonOk).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                doClick();
+
+            }
+        });
+    }
+
+    private void doClick() {
+        String nama = etNama.getText().toString();
+        String alamat = etAlamat.getText().toString();
+        String nomor = etNomor.getText().toString();
+
+        if (isValid()) {
+            tvHasil.setText("Nama : " + nama +
+                    "\nAlamat : " + alamat +
+                    "\nNomor Telepon : " + nomor);
+        }
+    }
+
+    private boolean isValid() {
+        boolean valid = true;
+
+        String namaa = etNama.getText().toString();
+        String alamatt = etAlamat.getText().toString();
+        String nomorr = etNomor.getText().toString();
+
+        if (namaa.isEmpty()) {
+            etNama.setError("Nama belum diisi");
+            valid = false;
+        } else if (alamatt.isEmpty()) {
+            etAlamat.setError("Alamat belum diisi");
+            valid = false;
+        } else if (nomorr.isEmpty()) {
+            etNomor.setError("Nomor telepon belum diisi");
+            valid = false;
+        } else {
+            etNama.setError(null);
+            etAlamat.setError(null);
+            etNomor.setError(null);
+        }
+
+        return valid;
+
     }
 }
